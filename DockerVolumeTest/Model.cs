44f46efb6b996e09a980b02d4d8ca41fb3a74052
reccesoft <87607185+reccesoft@users.noreply.github.com>
2021-10-05 19:12:26 +0200

    Just

---
+++ DockerVolumeTest/Model.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+using System.IO;
+using System.Linq;
+
+namespace DockerVolumeTest
+{
+    public class Model
+    {
+        IWebHostEnvironment WebHost;
+
+        public Model(IWebHostEnvironment webHost)
+        {
+            WebHost = webHost;
+        }
+
+        void Get()
+        {
+            string path = Path.Combine(WebHost.WebRootPath, "svmedia");
+            var files = Directory.EnumerateFiles(path);
+            if (files.Any())
+            {
+                try
+                {
+
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+
+
+}
